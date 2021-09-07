ecf4acb2864fb11fa52cdf7a580f7eb626a278f0
Andre John Louis Cura <ajlcura@hotmail.com>
2021-09-07 17:37:20 -0400

    formatted code back to using tabs

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,34 @@ use parser::Parser;
 use state::State;
 
 fn main() {
-    println!("「」 never loses");
-    let mut parser = Parser::new();
-    let mut engine = Engine::new();
-    loop {
-        if parser.read_input().is_err() {
-            continue;
-        }
-        let result = parser.parse_input();
-        match result {
-            Some(InputType::StateRequest(b, t)) => {
-                engine.state.update(b, t);
-            }
-            Some(InputType::MoveRequest) => {
-                let best_move = engine.get_best_move();
-                println!("{:?}", best_move);
-            }
-            Some(InputType::EvalRequest) => {
-                let mut best_move = 0;
-                let alpha = isize::MIN;
-                let beta = isize::MAX;
-                let w = engine.evaluate_state(0, &mut best_move, alpha, beta);
-                println!("{:?}", w);
-                println!("{} {} {}", best_move, alpha, beta);
-            }
-            Some(InputType::QuitRequest) => {
-                break;
-            }
-            None => {}
-        }
-    }
+	println!("「」 never loses");
+	let mut parser = Parser::new();
+	let mut engine = Engine::new();
+	loop {
+		if parser.read_input().is_err() {
+			continue;
+		}
+		let result = parser.parse_input();
+		match result {
+			Some(InputType::StateRequest(b, t)) => {
+				engine.state.update(b, t);
+			}
+			Some(InputType::MoveRequest) => {
+				let best_move = engine.get_best_move();
+				println!("{:?}", best_move);
+			}
+			Some(InputType::EvalRequest) => {
+				let mut best_move = 0;
+				let alpha = isize::MIN;
+				let beta = isize::MAX;
+				let w = engine.evaluate_state(0, &mut best_move, alpha, beta);
+				println!("{:?}", w);
+				println!("{} {} {}", best_move, alpha, beta);
+			}
+			Some(InputType::QuitRequest) => {
+				break;
+			}
+			None => {}
+		}
+	}
 }
